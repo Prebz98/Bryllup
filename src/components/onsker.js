079abcbb9ae0af1f034880061154473dcfa26b15
Preben <preben.zahl@gmail.com>
2023-06-08 09:50:54 +0200

    opplevelse + alkoholfritt

--- src/components/onsker.js
+++ src/components/onsker.js
@@ -10,6 +10,7 @@ function Onsker(props) {
             <div className="section-text">
                 <ul className="onskeliste">
                     <li>- Penger til bryllupsreise</li>
+                    <li>- Opplevelse</li>
                     <li>- Diverse til kjøkkenet, liste finnes <a href="https://www.kitchn.no/onskeliste/155715">her</a>. Husk å få markert dersom du har kjøpt noe. Dette kan noen anstatte gjøre dersom du kjøper i butikk.</li>
                 </ul>
             </div>
